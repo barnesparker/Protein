f6ba26dec1432d72ab61d84c23cba141b20004d8
Matthew Morgan <matt4byu@gmail.com>
2020-09-18 12:05:42 -0600

    Changing my files

--- MM_ProteinDataCleaning.R
+++ MM_ProteinDataCleaning.R
@@ -11,17 +11,22 @@ library(missForest)
 ### Load in data and create one tibble
 protein.train <- read_csv("ProteinTrain.csv")
 protein.test <- read_csv("ProteinTest.csv")
+
+# Merge the training and test data
 protein <- bind_rows(train = protein.train, test = protein.test, .id = "Set")
-# make Amino Acid a factor
-protein <- protein %>% 
-  mutate(Amino.Acid = as_factor(Amino.Acid))
+
+# Make the Amino.Acid and Response variables factors
+protein <- protein %>% mutate_at(vars(Amino.Acid, Response), factor)
 
 # Summary
 summary(protein)
 
-
-### Missing values
+# Plot missing values
 plot_missing(protein)
+# Consensus and PSSM have NAs
+
+# Table of Amino Acid and Response
+addmargins(table(protein$Amino.Acid, protein$Response))
 
 # Use random forests to fill NA values
 filled_nas <- protein %>% 
@@ -29,61 +34,14 @@ filled_nas <- protein %>%
   as.data.frame() %>% 
   missForest()
 
-# protein.c %>% 
-#  bind_rows(filled_nas %>% )
-# forest$ximp
-
 # merge Response and Set back with other variables
-protein.c <- filled_nas$ximp %>%  
-  merge(protein)
-
+protein.c <- filled_nas$ximp %>%  cbind(., protein %>% select(Set, Response)) %>% select(Set, SiteNum, Response, Amino.Acid, Iupred.score, ANN, PSSM, SVM, Consensus, normalization)
 
 # Correlation matrix
 plot_correlation(protein.c, type = "continuous", 
                  cor_args = list(use = "pairwise.complete.obs"))
 
-# Replace missing "Consensus" values with the mean of it's SVM and ANN values
-#protein.c <- protein %>%
-# rowwise() %>% 
-#  mutate(Consensus = replace_na(Consensus, mean(c(ANN, SVM)))) %>% 
-#  arrange(SiteNum)
-
-# Make linear model to impute missing PSSM values; regress on SVM & Consensus variables
-
-# model
-
-imp_lm <- lm(PSSM ~ SVM + Consensus, data = protein.c)
-
-# Impute predicted values into our data
-protein.c <- protein.c %>%
-  rowwise() %>% 
-  mutate(PSSM = replace_na(PSSM, predict(imp_lm, newdata = tibble(SVM, Consensus))))
-
-protein.test <- aregImpute(~ Consensus + SVM + normalization, data = protein.c)
-
-protein.test$imputed
-
-
-#imp_lm <- lm(PSSM ~ SVM+Consensus, data = protein.c)
-
-# Impute predicted values into our data
-#protein.c <- protein.c %>%
-#  rowwise() %>% 
-#  mutate(PSSM = replace_na(PSSM, predict(imp_lm, newdata = tibble(SVM, Consensus))))
-
-source("HelpfulFunctions.R")
-# Scatterplots with smoothers (Using a function I made found in HelpfulFunctions.R)
-sp1 <- protein %>% scp_smooth(SVM, Response, col = Amino.Acid)
-sp2 <- protein %>% scp_smooth(normalization, Response, col = Amino.Acid)
-sp3 <- protein %>% scp_smooth(Iupred.score, Response, col = Amino.Acid)
-
-grid.arrange(sp1, sp2, sp3, ncol = 2)
-
-# table of Amino Acid and Response
-addmargins(table(protein$Amino.Acid, protein$Response))
-
-### Visualization
-
+# Confirming that all NAs have been dealt with
 plot_missing(protein.c)
 
 # Histograms
